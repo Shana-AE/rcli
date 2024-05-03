f7955d2e0a831f35a02a21c85a607db2285aecf6
ShanaAE <Shana.wav@gmail.com>
2024-05-03 21:48:25 +0800

    feat: add command line options of subcommand csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Args, Parser, Subcommand};
+
+// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Args)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(long, default_value_t = false)]
+    header: bool,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+}
+
 fn main() {
-    println!("Hello, rust cli!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
