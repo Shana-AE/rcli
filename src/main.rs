793dd08995abe13308f80ef96400e92fc24f45ec
ShanaAE <Shana.wav@gmail.com>
2024-05-05 23:53:49 +0800

    refactor: extract csv_processor to seperated module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,12 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 use clap::Parser;
-use csv::Reader;
-use rcli::{Opts, SubCommand};
-use serde::{Deserialize, Serialize};
-use std::fs;
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+use rcli::{process_csv, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     println!("{:?}", opts);
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.deserialize() {
-                let record: Player = result?;
-                ret.push(record);
-            }
-            let json = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-        }
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
     Ok(())
 }
