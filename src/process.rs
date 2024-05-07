af534b1d39d2c71a9f47fc90f098bb2e7214dfe9
ShanaAE <Shana.wav@gmail.com>
2024-05-07 22:27:50 +0800

    refactor: just use serde_json::Value to serialize, because serde_xxx only requires Serialize and Sized trait

--- src/process.rs
+++ src/process.rs
@@ -19,46 +19,31 @@ struct Player {
 pub fn process_csv(input: &str, output: &str, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let headers = reader.headers()?.clone();
+    let mut ret = Vec::with_capacity(128);
+    for result in reader.records() {
+        let record = result?;
+        let record = headers
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
+        ret.push(record);
+    }
     let content = match format {
-        OutputFormat::Json => {
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.records() {
-                let record = result?;
-                let iter = headers.iter().zip(record.iter());
-                let json_record = iter.collect::<serde_json::Value>();
-                ret.push(json_record);
-            }
-            serde_json::to_string_pretty(&ret)?
-        }
-        OutputFormat::Yaml => {
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.records() {
-                let record = result?;
-                let iter = headers.iter().zip(record.iter());
-                let yaml_record = serde_yaml::to_value(iter.collect::<serde_json::Value>())?;
-                ret.push(yaml_record);
-            }
-            serde_yaml::to_string(&ret)?
-        }
         OutputFormat::Toml => {
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.records() {
-                let record = result?;
-                let iter = headers.iter().zip(record.iter());
-                let toml_record = iter.collect::<HashMap<_, _>>();
-                let toml_record: toml::Value = toml_record.into();
-                ret.push(toml_record)
-            }
-            let mut result = HashMap::new();
-
             let file: Vec<_> = input.split(std::path::MAIN_SEPARATOR).collect();
             let input = file[file.len() - 1];
-            let input = input.split('.').collect::<Vec<&str>>();
-            let filename = input[0];
+            let filename: Vec<_> = input.split('.').collect();
+            let filename = filename[0];
+            let mut result = HashMap::new();
             result.insert(filename, ret);
-            toml::to_string_pretty(&result)?
+            toml::to_string(&result)?
         }
+        // because serde_yaml::to_string() only requires Serialize and Sized trait, so we can use serde_json::Value here
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+        // because serde_yaml::to_string() only requires Serialize and Sized trait, so we can use serde_json::Value here
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
     };
+    println!("content: {}", content);
     fs::write(output, content)?;
     Ok(())
 }
