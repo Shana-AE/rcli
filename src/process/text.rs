c8a971f5e0549b6b510a14884a9a9436187015ef
ShanaAE <Shana.wav@gmail.com>
2024-05-14 00:06:57 +0800

    feat: add sign of ed25519

--- src/process/text.rs
+++ src/process/text.rs
@@ -1,11 +1,18 @@
 use std::{fs, io::Read};
 
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
 
 use crate::{get_reader, TextSignFormat};
 
 pub trait TextSign {
+    /// Sign the data from the reader and return the signature
     fn sign(&self, reader: &mut dyn Read) -> anyhow::Result<Vec<u8>>;
+}
+
+pub trait TextVerify {
+    /// Verify the signature against the data from the reader
+    fn verify(&self, reader: impl Read, sig: &[u8]) -> anyhow::Result<bool>;
 }
 
 struct Blake3 {
@@ -22,24 +29,38 @@ impl TextSign for Blake3 {
     }
 }
 
-// struct Ed25519Signer {
-//     key: [u8; 32],
-// }
-
-// struct Ed25519Verifier {
-//     key: [u8; 32],
-// }
-
-pub trait TextVerify {
-    fn verify(&self, reader: impl Read, sig: &[u8]) -> anyhow::Result<bool>;
-}
-
 impl TextVerify for Blake3 {
     fn verify(&self, mut reader: impl Read, sig: &[u8]) -> anyhow::Result<bool> {
         let mut buf = Vec::new();
         reader.read_to_end(&mut buf)?;
         let hash = blake3::hash(&buf);
         Ok(hash.as_bytes() == sig)
+    }
+}
+
+struct Ed25519Signer {
+    key: SigningKey,
+}
+
+struct Ed25519Verifier {
+    key: VerifyingKey,
+}
+
+impl TextSign for Ed25519Signer {
+    fn sign(&self, reader: &mut dyn Read) -> anyhow::Result<Vec<u8>> {
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
+        let sig = self.key.sign(&buf).to_bytes().to_vec();
+        Ok(sig)
+    }
+}
+
+impl TextVerify for Ed25519Verifier {
+    fn verify(&self, mut reader: impl Read, sig: &[u8]) -> anyhow::Result<bool> {
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
+        let sig = Signature::from_bytes(sig.try_into()?);
+        Ok(self.key.verify(&buf, &sig).is_ok())
     }
 }
 
@@ -53,7 +74,13 @@ pub fn process_text_sign(input: &str, key: &str, format: TextSignFormat) -> anyh
             let signer = Blake3 { key };
             signer.sign(&mut reader)?
         }
-        TextSignFormat::Ed25519 => todo!(),
+        TextSignFormat::Ed25519 => {
+            let key = fs::read(key)?;
+            let key = &key[..32];
+            let key = SigningKey::from_bytes(key.try_into()?);
+            let signer = Ed25519Signer { key };
+            signer.sign(&mut reader)?
+        }
     };
     let signed = URL_SAFE_NO_PAD.encode(signed);
     println!("{}", signed);
